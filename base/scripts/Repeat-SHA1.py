736e98eba28acf2212756b273b2ab92ee0718d43
Samat K Jain <samat@samat.org>
2018-07-13 02:47:02 -0700

    Repeat-SHA1: Jobs/processes parameter

--- base/scripts/Repeat-SHA1.py
+++ base/scripts/Repeat-SHA1.py
@@ -40,9 +40,10 @@ def foobar(filename, digest):
 
 
 @click.command()
-@click.option('-n', '--number', help="Number of times to repeat, default=3", default=3, type=click.IntRange(min=0))
+@click.option('-n', '--number', help="Number of times to repeat. default=3", default=3, type=click.IntRange(min=0))
+@click.option('-j', '--jobs', help="Number of jobs/processes. Should be function of I/O bandwidth, and not processor cores. default=2", default=2, type=click.IntRange(min=0))
 @click.argument('digest_files', type=click.Path(readable=True), nargs=-1)
-def main(digest_files, number):
+def main(digest_files, number, jobs):
     filenames_to_digest = {}
 
     for current_digest_file in digest_files:
@@ -53,7 +54,7 @@ def main(digest_files, number):
                 digest, filename = line[0].strip(), line[1].strip()
                 filenames_to_digest[filename] = digest
 
-    with multiprocessing.Pool(2) as pool:
+    with multiprocessing.Pool(jobs) as pool:
         return_vals = pool.starmap(foobar, filenames_to_digest.items())
 
     return all(return_vals)
