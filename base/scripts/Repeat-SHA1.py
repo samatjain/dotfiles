f4f81888521a36fca812acfbf4984ada775a61cd
Samat K Jain <_@skj.io>
2018-10-04 20:56:31 -0700

    Repeat-SHA1: Add new `--delete` and `--number` options
     - Add new `--delete` option, will delete files w/ failed checksums
     - The `--number` option now actually works

--- base/scripts/Repeat-SHA1.py
+++ base/scripts/Repeat-SHA1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import hashlib
 import multiprocessing
 import os
@@ -11,8 +12,8 @@ from pprint import pprint
 import click
 
 
-def foobar(filename, digest):
-    for i in range(3):
+def PerformChecksum(filename, digest, delete=False, repeat=3):
+    for _ in range(repeat):
         try:
             with open(filename, "rb") as f:
                 h = hashlib.new('sha1')
@@ -35,16 +36,24 @@ def foobar(filename, digest):
             print()
             sys.exit(130)
 
-    print('FAILED:', filename)
+    if delete:
+        print('DELETE:', filename)
+        os.unlink(filename)
+    else:
+        print('FAILED:', filename)
+
     return False
 
 
 @click.command()
+@click.option('--delete', help="Delete file if checksum fails. default=False", is_flag=True, default=False)
 @click.option('-n', '--number', help="Number of times to repeat. default=3", default=3, type=click.IntRange(min=0))
 @click.option('-j', '--jobs', help="Number of jobs/processes. Should be function of I/O bandwidth, and not processor cores. default=2", default=2, type=click.IntRange(min=0))
 @click.argument('digest_files', type=click.Path(readable=True), nargs=-1)
-def main(digest_files, number, jobs):
+def main(digest_files, delete, number, jobs):
     filenames_to_digest = {}
+
+    fnChecksum = functools.partial(PerformChecksum, delete=delete, repeat=number)
 
     for current_digest_file in digest_files:
         current_digest_file = os.path.abspath(os.path.expanduser(current_digest_file))
@@ -55,7 +64,7 @@ def main(digest_files, number, jobs):
                 filenames_to_digest[filename] = digest
 
     with multiprocessing.Pool(jobs) as pool:
-        return_vals = pool.starmap(foobar, filenames_to_digest.items())
+        return_vals = pool.starmap(fnChecksum, filenames_to_digest.items())
 
     return all(return_vals)
 
