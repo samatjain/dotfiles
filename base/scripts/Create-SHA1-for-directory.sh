35695189ee69cc7c6fea411c18552a447e31e9fd
Samat K Jain <_@skj.io>
2018-11-20 23:07:23 -0800

    Create-SHA1-for-directory.sh: Exclude the file we're writing to
    When the script's argument is ".", we don't want to checksum the file we
    are writing out to.

--- base/scripts/Create-SHA1-for-directory.sh
+++ base/scripts/Create-SHA1-for-directory.sh
@@ -10,10 +10,11 @@ for i in $@; do
 	# If path argument is ".", then use CWD instead
 	if [ $i = "." ]; then
 		dst=$(basename $(pwd))
+		echo "# WARNING: Remove this file"
 	fi
 	if [ $GNU_PARALLEL_PRESENT -eq 1 ]; then
-		echo "find $i -type f -print0 | parallel -q0 -k sha1sum > $dst.sha1"
+		echo "find $i -type f ! -name \"$dst.sha1\" -print0 | parallel -q0 -k sha1sum > $dst.sha1"
 	else
-		echo "find $i -type f -exec sha1sum '{}' \; > $dst.sha1"
+		echo "find $i -type f ! -name \"$dst.sha1\" -exec sha1sum '{}' \; > $dst.sha1"
 	fi
 done
