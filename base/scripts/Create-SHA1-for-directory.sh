f446e186402884fa9346a2309c8dcb3ccfa91740
Samat K Jain <_@skj.io>
2018-11-20 23:01:39 -0800

    Create-SHA1-for-directory.sh: Better support for "." as an argument

--- base/scripts/Create-SHA1-for-directory.sh
+++ base/scripts/Create-SHA1-for-directory.sh
@@ -6,9 +6,14 @@
 GNU_PARALLEL_PRESENT=$?
 for i in $@; do
 	i=$(basename $i)
+	dst=$i
+	# If path argument is ".", then use CWD instead
+	if [ $i = "." ]; then
+		dst=$(basename $(pwd))
+	fi
 	if [ $GNU_PARALLEL_PRESENT -eq 1 ]; then
-		echo "find $i -type f -print0 | parallel -q0 -k sha1sum > $i.sha1"
+		echo "find $i -type f -print0 | parallel -q0 -k sha1sum > $dst.sha1"
 	else
-		echo "find $i -type f -exec sha1sum '{}' \; > $i.sha1"
+		echo "find $i -type f -exec sha1sum '{}' \; > $dst.sha1"
 	fi
 done
