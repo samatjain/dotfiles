3b2765d6f100265500237eea2a74a539bc880b67
Samat K Jain <_@skj.io>
2018-11-20 22:49:44 -0800

    Create-SHA1-for-directory.sh: Use GNU parallel if available
    I suck at bash.

--- base/scripts/Create-SHA1-for-directory.sh
+++ base/scripts/Create-SHA1-for-directory.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+# Check if GNU parallel is installed
+# TODO: Actually check if GNU parallel is installed, rather than a command
+#   called `parallel`
+! hash parallel 2> /dev/null
+GNU_PARALLEL_PRESENT=$?
 for i in $@; do
 	i=$(basename $i)
-	echo "find $i -type f -exec sha1sum '{}' \; > $i.sha1"
+	if [ $GNU_PARALLEL_PRESENT -eq 1 ]; then
+		echo "find $i -type f -print0 | parallel -q0 -k sha1sum > $i.sha1"
+	else
+		echo "find $i -type f -exec sha1sum '{}' \; > $i.sha1"
+	fi
 done
