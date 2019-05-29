2af40e3652a0c291a563903edbc0505f5c8e60e7
Samat K Jain <_@skj.io>
2019-05-29 01:07:01 -0700

    Repeat-SHA1: Tons of new stuff
    - New interactive (mode default) will use threads instead of processes.
      While *much* slower, you know get a nice progress bar and COLORS!
    - Non-interactive mode, called batch mode, uses processes, can be 10x
      faster, and no colors.
    - Rename long option "number" to "repeat"

--- base/scripts/Repeat-SHA1.py
+++ base/scripts/Repeat-SHA1.py
@@ -6,17 +6,28 @@ from __future__ import unicode_literals
 
 import functools
 import hashlib
-import multiprocessing
-import os
+import io
 import itertools
-
+import os
+import sys
 from io import DEFAULT_BUFFER_SIZE
-from pprint import pprint
+from pprint import pprint  # NOQA
 
 import click
+from colorama import Fore
+from tqdm import tqdm
 
 
-def PerformChecksum(filename, digest, delete=False, repeat=3):
+def PerformChecksum(filename, digest, delete=False, repeat=3, pbar=None, is_interactive=False) -> bool:
+    # If we're not in batch (i.e. interactive) mode, then use colors and
+    # update progress bar. Otherwise, use stdout.
+    if is_interactive:
+        out = tqdm
+        pbar.update(1)
+    else:
+        out = sys.stdout
+
+    out_fp = io.StringIO()  # File-like for output from this thread
     for _ in range(repeat):
         try:
             with open(filename, "rb") as f:
@@ -34,30 +45,48 @@ def PerformChecksum(filename, digest, delete=False, repeat=3):
 
         except (IOError, OSError) as e:
             if e.errno != 32: # Ignore EPIPE (broken pipe)
-                print('ERROR:', filename)
+                click.echo(click.style('ERROR', fg='yellow'), out_fp, nl=False, color=is_interactive)
+                click.echo(': {filename}'.format(filename=filename), out_fp, nl=False)
+                tqdm.write(out_fp.getvalue())
                 return False
         except KeyboardInterrupt:
             print()
             sys.exit(130)
 
     if delete:
-        print('DELETE:', filename)
+        click.echo(click.style('DELETE', fg='magenta'), out_fp, nl=False, color=is_interactive)
+        click.echo(': {filename}'.format(filename=filename), out_fp, nl=False)
         os.unlink(filename)
     else:
         print('FAILED:', filename)
+        click.echo(click.style('FAILED', fg='red'), out_fp, nl=False, color=is_interactive)
+        click.echo(': {filename}'.format(filename=filename), out_fp, nl=False)
 
+    out.write(out_fp.getvalue())
     return False
 
 
 @click.command()
 @click.option('--delete', help="Delete file if checksum fails. default=False", is_flag=True, default=False)
-@click.option('-n', '--number', help="Number of times to repeat. default=3", default=3, type=click.IntRange(min=0))
+@click.option('--batch', 'batch_mode', help="Batch mode hides progress bar and colors. default=False", is_flag=True, default=False)
+@click.option('-n', '--repeat', help="Number of times to repeat. default=3", default=3, type=click.IntRange(min=0))
 @click.option('-j', '--jobs', help="Number of jobs/processes. Should be function of I/O bandwidth, and not processor cores. default=2", default=2, type=click.IntRange(min=0))
 @click.argument('digest_files', type=click.Path(readable=True), nargs=-1)
-def main(digest_files, delete, number, jobs):
+def main(digest_files, delete, batch_mode, repeat, jobs):
     filenames_to_digest = {}
 
-    fnChecksum = functools.partial(PerformChecksum, delete=delete, repeat=number)
+    # Interactive mode?
+    try:
+        is_interactive = sys.stdout.isatty()
+    except:
+        is_interactive = False
+    if batch_mode:
+        is_interactive = False
+
+    if is_interactive:
+        import multiprocessing.dummy as multiprocessing
+    else:
+        import multiprocessing
 
     for current_digest_file in digest_files:
         current_digest_file = os.path.abspath(os.path.expanduser(current_digest_file))
@@ -67,8 +96,25 @@ def main(digest_files, delete, number, jobs):
                 digest, filename = line[0].strip(), line[1].strip()
                 filenames_to_digest[filename] = digest
 
+    total_files = len(filenames_to_digest)
+    pbar = None
+    if is_interactive:
+        click.echo('Checksumming {total_files} file(s) from {total_digests} digest(s).'.format(total_files=total_files, total_digests=len(digest_files)))
+        pbar = tqdm(
+            total=total_files,
+            bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET),
+            unit="files",
+            dynamic_ncols=True,
+            position=0)
+
+    fnChecksum = functools.partial(PerformChecksum, delete=delete, repeat=repeat, pbar=pbar, is_interactive=is_interactive)
+
+    #with multiprocessing.dummy.Pool(jobs, initializer=tqdm.set_lock, initargs=(RLock(),)) as pool:
     with multiprocessing.Pool(jobs) as pool:
         return_vals = pool.starmap(fnChecksum, filenames_to_digest.items())
+
+    if is_interactive:
+        pbar.close()
 
     return all(return_vals)
 
