34c9389416c594f3e06f1345cdf7e3611bfd3601
Samat K Jain <_@skj.io>
2019-07-22 17:16:57 -0700

    Repeat-SHA1: Change default number of jobs 2 → 4

--- base/scripts/Repeat-SHA1.py
+++ base/scripts/Repeat-SHA1.py
@@ -70,7 +70,7 @@ def PerformChecksum(filename, digest, delete=False, repeat=3, pbar=None, is_inte
 @click.option('--delete', help="Delete file if checksum fails. default=False", is_flag=True, default=False)
 @click.option('--batch', 'batch_mode', help="Batch mode hides progress bar and colors. default=False", is_flag=True, default=False)
 @click.option('-n', '--repeat', help="Number of times to repeat. default=3", default=3, type=click.IntRange(min=0))
-@click.option('-j', '--jobs', help="Number of jobs/processes. Should be function of I/O bandwidth, and not processor cores. default=2", default=2, type=click.IntRange(min=0))
+@click.option('-j', '--jobs', help="Number of jobs/processes. Should be function of I/O bandwidth, and not processor cores. default=4", default=4, type=click.IntRange(min=0))
 @click.argument('digest_files', type=click.Path(readable=True), nargs=-1)
 def main(digest_files, delete, batch_mode, repeat, jobs):
     filenames_to_digest = {}
