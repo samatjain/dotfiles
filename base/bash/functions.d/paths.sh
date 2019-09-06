2276c4c919b996852afc33c8ba6ffe568fd9f993
Samat K Jain <_@skj.io>
2019-09-06 13:57:19 -0700

    bash: cd alias, prints new CWD on directory change

--- base/bash/functions.d/paths.sh
+++ base/bash/functions.d/paths.sh
@@ -1,5 +1,17 @@
 # Functions related to paths
 
+# Print working directory after cd
+# zsh compatible
+# From: https://node.mu/2010/11/24/is-your-bash-prompt-cramping-your-style/
+cd() {
+    if [[ $@ == '-' ]]; then
+        builtin cd "$@" > /dev/null  # We'll handle pwd.
+    else
+        builtin cd "$@"
+    fi
+    echo -e "    \033[1;30m"`builtin pwd`"\033[0m"
+}
+
 # CWD's basename
 # zsh compatible
 bpwd() {
