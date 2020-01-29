f43a70d1de0a6eafa7221a41670b38136e3de72f
Samat K Jain <_@skj.io>
2020-01-29 01:08:35 -0800

    Create-SHA1: add GNU parallel and xxh support

--- base/scripts/Create-SHA1-Singleton.sh
+++ base/scripts/Create-SHA1-Singleton.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# Check if xxhsum is installed
+! hash xxhsum 2> /dev/null
+_XXH_PRESENT=$?
 for i in $@; do
-	i=$(basename $i)
-	sha1sum $i > $i.sha1
+	i=$(basename "$i")
+	sha1sum "$i" > "$i".sha1
+	if [ $_XXH_PRESENT -eq 1 ]; then
+		xxhsum -q "$i" > "$i".xxh
+	fi
 done
--- base/scripts/Create-SHA1-for-directory.sh
+++ base/scripts/Create-SHA1-for-directory.sh
@@ -3,7 +3,10 @@
 # TODO: Actually check if GNU parallel is installed, rather than a command
 #   called `parallel`
 ! hash parallel 2> /dev/null
-GNU_PARALLEL_PRESENT=$?
+_GNU_PARALLEL_PRESENT=$?
+# Check if xxhsum is installed
+! hash xxhsum 2> /dev/null
+_XXH_PRESENT=$?
 for i in $@; do
 	i=$(basename $i)
 	dst=$i
@@ -12,9 +15,16 @@ for i in $@; do
 		dst=$(basename $(pwd))
 		echo "# WARNING: Remove this file"
 	fi
-	if [ $GNU_PARALLEL_PRESENT -eq 1 ]; then
-		echo "find \"$i\" -type f ! -name \"$dst.sha1\" -print0 | parallel -q0 -k sha1sum > \"$dst.sha1\""
+	if [ $_GNU_PARALLEL_PRESENT -eq 1 ]; then
+		echo "find \"$i\" -type f ! -name \"$dst.sha1\" ! -name \"$dst.xxh\" -print0 | parallel -q0 -k sha1sum > \"$dst.sha1\""
+		if [ $_XXH_PRESENT -eq 1 ]; then
+			echo "find \"$i\" -type f ! -name \"$dst.sha1\" ! -name \"$dst.xxh\" -print0 | parallel -q0 -k xxhsum -q > \"$dst.xxh\""
+		fi
 	else
-		echo "find \"$i\" -type f ! -name \"$dst.sha1\" -exec sha1sum '{}' \; > \"$dst.sha1\""
+		echo "find \"$i\" -type f ! -name \"$dst.sha1\" ! -name \"$dst.xxh\" -exec sha1sum '{}' \; > \"$dst.sha1\""
+		if [ $_XXH_PRESENT -eq 1 ]; then
+			# If we don't have GNU parallel, assume we're using an old xxhsum w/o -q too
+			echo "find \"$i\" -type f ! -name \"$dst.sha1\" ! -name \"$dst.xxh\" -exec xxhsum -q '{}' \; > \"$dst.xxh\""
+		fi
 	fi
 done
