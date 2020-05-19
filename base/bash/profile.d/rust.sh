eec378fb7376e06591af7f0237882ab73692e275
Samat K Jain <_@skj.io>
2020-05-18 21:29:07 -0700

    bash: support for cargo on debian, shellcheck fixes

--- base/bash/profile.d/rust.sh
+++ base/bash/profile.d/rust.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Source default install directory for rustup.rs and cargo
-[[ -f "$HOME/.cargo/env" ]] && source $HOME/.cargo/env
+[[ -f "${HOME}/.cargo/env" ]] && source "${HOME}/.cargo/env"
+# Debian-installed cargo doesn't create the above env file, so manually add to path
+[[ ! -f "${HOME}/.cargo/env" ]] && [[ -d "${HOME}/.cargo/bin" ]] && export PATH="${HOME}/.cargo/bin:${PATH}"
