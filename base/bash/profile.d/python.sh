cbd9fd71cd4f7e928063309be34bee206b4512c4
Samat K Jain <_@skj.io>
2020-06-22 15:03:21 -0700

    bash: set PIP_CONFIG_FILE for pip3
    Ubuntu 20.04 no longer has pip; set this env if pip3 exists.

--- base/bash/profile.d/python.sh
+++ base/bash/profile.d/python.sh
@@ -1,2 +1,3 @@
 # Configuration file for pip
 hash pip 2> /dev/null && export PIP_CONFIG_FILE="$XDG_CONFIG_HOME/pip/pip.conf"
+hash pip3 2> /dev/null && export PIP_CONFIG_FILE="$XDG_CONFIG_HOME/pip/pip.conf"
