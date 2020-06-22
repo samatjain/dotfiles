de1ac61e3d02f46c98d40ff05f989347a86b874a
Samat K Jain <_@skj.io>
2020-06-22 15:04:59 -0700

    bash: more npm XDG tweaks

--- base/bash/profile.d/npm.sh
+++ base/bash/profile.d/npm.sh
@@ -1,6 +1,12 @@
 # Configuration file for nodejs' npm
+if hash node 2>/dev/null
+then
+	export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+fi
+
 if hash npm 2>/dev/null
 then
 	export PATH="${HOME}/.local/lib/nodejs/bin:${PATH}"
 	export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+	export NPM_CONFIG_DEVDIR="$XDG_CACHE_HOME/node-gyp"
 fi
