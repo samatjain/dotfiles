10d9e20a9b5f637713e3003d6bdce358fa2ecf39
Samat K Jain <_@skj.io>
2025-12-16 16:02:56 -0800

    bash: add ${XDG_BIN_HOME}

--- base/bash/rc.d/01-xdg.sh
+++ base/bash/rc.d/01-xdg.sh
@@ -4,3 +4,5 @@ export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export  XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export   XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export  XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+# non-standard
+export    XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}"
