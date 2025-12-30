4d851506ebe76c3e027df02a161c5bb50530e260
Samat K Jain <_@skj.io>
2025-12-30 11:25:53 -0800

    Update-Locally-Installed.sh: remove rye (uv replaces all use cases)

--- base/scripts/Update-Locally-Installed.sh
+++ base/scripts/Update-Locally-Installed.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-# { cargo install --git https://github.com/astral-sh/rye rye --force; } &
 { pipx upgrade-all --include-injected --verbose; } &
 { cargo install-update --all; } &
 { uv tool upgrade --all; } &
